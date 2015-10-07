acb0b86eec9641fcb3ca300803d176f4eb7ffd66
Kevin Utoft <kevin.utoft@gmail.com>
2015-10-06 20:45:29 -0500

    create starter project folder

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+(function($, win){
+
+$(document).ready(function () {
+
+    // =====
+    // Execute this JS after page content has loaded (images, videos, etc):
+    // =====
+    $(window).bind("load", function() {
+
+        $(window).resize(function() {
+        	
+        });
+    
+    });
+	
+});
+
+})( jQuery, this); //-- end jQuery/AUI
